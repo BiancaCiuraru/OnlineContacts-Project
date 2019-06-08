08da7122897d6485a240c1aea5986c25e8c9e09f
biancaciuraru <biancaciuraru@yahoo.com>
2019-06-08 04:11:29 +0300

    Add login session

--- src/pages/register.controller.php
+++ src/pages/register.controller.php
@@ -1,5 +1,6 @@
-<?php 
+<?php
     session_start();
+
     include_once 'register.model.php';
     $register = NULL;
     $registerStatus = false;
@@ -9,28 +10,38 @@
     $passwordRules = true;
     $loginStatus = null;
     $fieldsStatus = true;
-    if(isset($_POST['submit'])) {
-        if($_POST['submit']==='registerButton'){
-            if(!$_POST['Fname'] || !$_POST['Lname'] || !$_POST['email'] || !$_POST['password1'] || !$_POST['password2']){
+    if (isset($_POST['submit'])) {
+        if ($_POST['submit'] === 'registerButton') {
+            if (!$_POST['Fname'] || !$_POST['Lname'] || !$_POST['email'] || !$_POST['password1'] || !$_POST['password2']) {
                 $fieldsStatus = false;
-            }else if(!nameValidity($_POST['Fname'], $_POST['Lname'], $_POST['email'])){
+            } else if (!nameValidity($_POST['Fname'], $_POST['Lname'], $_POST['email'])) {
                 $registerName = false;
-            // }else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-            //     $registerEmail = false;
-            }else if( !passwordValidity($_POST['password1'], $_POST['password2'])){
+                // }else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                //     $registerEmail = false;
+            } else if (!passwordValidity($_POST['password1'], $_POST['password2'])) {
                 $registerPassword = false;
-            }else if(!passwordRules($_POST['password1'], $_POST['password2'])){
+            } else if (!passwordRules($_POST['password1'], $_POST['password2'])) {
                 $passwordRules = false;
-            }else{
-                if($em = emailValidity($_POST['email'])>0){
+            } else {
+                if ($em = emailValidity($_POST['email']) > 0) {
                     $registerEmail = false;
-                }else{
+                } else {
                     $register = register($_POST['Fname'], $_POST['Lname'], $_POST['email'], $_POST['password1'], $_POST['password2']);
                     $registerStatus = true;
                     // header('Location: ./register.controller.php#openModal');
                 }
             }
+        } else if ($_POST['submit'] === 'loginButton') {
+            $user = login($_POST['emailLogin'], $_POST['password']);
+            if ($user !== NULL) {
+                header('Location: ../index.php');
+                $_SESSION['emailLogin'] = $user -> emailLogin;
+                $_SESSION['hashedPassword'] = $user -> hashedPassword;
+            } else {
+                $loginStatus = false;
+            }
         }
     }
+    
     include_once 'login.register.php';
 ?>
